3c1693d7a1bcee4288fc38fcfed8ab5483a74a00
Nikolay Kardash <nkardash@ozon.ru>
2025-03-23 18:51:34 +0300

    New tools.

--- install.sh
+++ install.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 
-brew install fzf
+
+brew install \
+	fzf \
+	tmux \
+	bat \
+	jq \
+	xh \
+	ripgrep \
+	git \
+	the_silver_searcher \
+	fd \
+	stylua \
+    bufbuild/buf/buf \
+    pgformatter \
+    ast-grep \
+    semgrep \
+    llvm \
+    zig \
+    zls
+
+
 
 brew update
 brew upgrade
+
+
+go install github.com/shurcooL/markdownfmt@latest
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
+go install -v github.com/quasilyte/go-ruleguard/cmd/ruleguard@latest 
+go install gotest.tools/gotestsum@latest
+go install github.com/nametake/golangci-lint-langserver@latest
